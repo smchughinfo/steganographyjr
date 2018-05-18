dc3ee23704177419fdf546dd31ee79bc4d47f526
Sean McHugh <seanmchugh.info@gmail.com>
2018-05-17 21:36:18 -0400

    continue work on layout

--- SteganographyJr/SteganographyJr/Views/SteganographyJr.xaml.cs
+++ SteganographyJr/SteganographyJr/Views/SteganographyJr.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,14 @@ using Xamarin.Forms.Xaml;
 
 namespace SteganographyJr.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+	//[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SteganographyJr : ContentPage
 	{
 		public SteganographyJr ()
 		{
 			InitializeComponent ();
-		}
+            var assembly = (typeof(SteganographyJr)).GetTypeInfo().Assembly;
+            carrierImage.Source = ImageSource.FromResource("SteganographyJr.Images.pic.jpg", assembly);
+        }
 	}
 }
