bd147bd74770fabfe9b952cdd3aa25491daea1d5
Sean McHugh <seanmchugh.info@gmail.com>
2018-05-19 12:18:33 -0400

    move mode to viewmodel

---
+++ SteganographyJr/SteganographyJr/StaticVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteganographyJr
+{
+    static class StaticVariables
+    {
+        public enum Modes { Encode, Decode };
+    }
+}
---
+++ SteganographyJr/SteganographyJr/ViewModels/SteganographyJr.cs
@@ -0,0 +1,53 @@
+﻿using SteganographyJr.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace SteganographyJr.ViewModels
+{
+    class SteganographyJr : INotifyPropertyChanged
+    {
+        List<Mode> modes;
+        Mode selectedMode;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public SteganographyJr()
+        {
+            Modes = new List<Mode>()
+            {
+                new Mode() { Key=StaticVariables.Modes.Encode, Value="Encode"},
+                new Mode() { Key=StaticVariables.Modes.Decode, Value="Decode"}
+            };
+            SelectedMode = Modes.Single(m => m.Key == StaticVariables.Modes.Encode);
+        }
+
+        public List<Mode> Modes
+        {
+            get
+            {
+                return modes;
+            }
+            set
+            {
+                modes = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Modes"));
+            }
+        }
+
+        public Mode SelectedMode
+        {
+            get
+            {
+                return selectedMode;
+            }
+            set
+            {
+                selectedMode = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SelectedMode"));
+            }
+        }
+    }
+}
