6e8c378b2b9cdd47ad1b29f85ac9405595eb27f0
seanmchughinfo <seanmchugh.info@gmail.com>
2018-06-26 21:15:47 -0400

    add test java implementation

---
+++ App/SteganographyJr/SteganographyJr.Android/MCW/Implementation/src/Utilities/IO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilities;
+
+/**
+ *
+ * @author Sean
+ */
+public class IO {
+    public int Add(int a, int b) {
+        return a + b;
+    }
+}
